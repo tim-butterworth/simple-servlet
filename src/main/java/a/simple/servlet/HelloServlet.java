b795505c37df6233b8fe06365a09a9910974ee04
Tim <timothy.d.butterworth@gmail.com>
2019-04-06 17:11:42 -0600

    Removes rabbit and the plugin for automatically running the war on embedded tomcat

--- src/main/java/a/simple/servlet/HelloServlet.java
+++ src/main/java/a/simple/servlet/HelloServlet.java
@@ -1,7 +1,5 @@
 package a.simple.servlet;
 
-import com.rabbitmq.client.*;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,36 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.concurrent.TimeoutException;
 
 @WebServlet(name = "CoreServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost("localhost");
-
-        try (Connection connection = connectionFactory.newConnection()) {
-            Channel channel = connection.createChannel();
-            channel.queueDeclare("customqueue", true, false, false, null);
-
-            channel.basicPublish("customexchange", "pdfprocess", null, "mymessage".getBytes());
-
-            DefaultConsumer callback = new DefaultConsumer(channel);
-            channel.basicConsume("customqueue", new DeliverCallback() {
-                @Override
-                public void handle(String consumerTag, Delivery message) throws IOException {
-                    String messageBody = new String(message.getBody());
-                    System.out.println("This is a message -> " + messageBody);
-                }
-            }, consumerTag -> { });
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("In service!!");
-
         super.service(req, resp);
     }
 
